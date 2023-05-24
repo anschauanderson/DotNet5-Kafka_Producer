8b01d3a983d4ba821bd24b0287821175bfb5eaa1
Anderson Anschau <anderson.anschau@inlog.com.br>
2023-05-24 17:57:55 -0300

    poc kfka producer

--- EnvioKafka/Program.cs
+++ EnvioKafka/Program.cs
@@ -1,64 +1,83 @@
-﻿using System;
+﻿using Confluent.Kafka;
+using Confluent.Kafka.Admin;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
-using Confluent.Kafka;
-using Serilog;
-using Serilog.Sinks.SystemConsole.Themes;
 
 namespace EnvioKafka
 {
     class Program
     {
+        public static Random generate = new Random();
+
         static async Task Main(string[] args)
         {
-            var logger = new LoggerConfiguration()
-                .WriteTo.Console(theme: AnsiConsoleTheme.Literate)
-                .CreateLogger();
-            logger.Information("Testando o envio de mensagens com Kafka");
+            string bootstrapServers = "localhost:9092";
+            string topicName = "topic-test";
+            int numeroParticoes = 10;
+            int qtdeRastreadores = 20;
+            int qtdePacotes = 500;
 
-            if (args.Length < 3)
+            await CreateTopic(bootstrapServers, topicName, numeroParticoes);
+
+            var rastreadores = new List<RastreadorEvent>();
+
+            Random random = new Random();
+
+            for (int i = 0; i < qtdeRastreadores; i++)
             {
-                logger.Error(
-                    "Informe ao menos 3 parâmetros: " +
-                    "no primeiro o IP/porta para testes com o Kafka, " +
-                    "no segundo o Topic que receberá a mensagem, " +
-                    "já no terceito em diante as mensagens a serem " +
-                    "enviadas a um Topic no Kafka...");
-                return;
+                rastreadores.Add(new RastreadorEvent(random.Next(1111, 999999)));
             }
+            var config = new ProducerConfig { BootstrapServers = bootstrapServers };
 
-            string bootstrapServers = args[0];
-            string nomeTopic = args[1];
+            using (var producer = new ProducerBuilder<Null, string>(config).Build())
+            {
+                for (int i = 0; i < qtdePacotes; i++)
+                {
+                    Console.WriteLine($"Pacote: {i}/{qtdePacotes}");
+                    int randomIndex = random.Next(0, rastreadores.Count);
 
-            logger.Information($"BootstrapServers = {bootstrapServers}");
-            logger.Information($"Topic = {nomeTopic}");
+                    var randomItem = rastreadores[randomIndex];
+                    randomItem.Incrementa();
+                    int lastDigit = (int)(randomItem.Rastreador % numeroParticoes);
 
+                    await EnviarMensagem(producer, topicName, lastDigit, randomItem);
+                }
+            }
+        }
+
+        static async Task EnviarMensagem(IProducer<Null, string> producer, string topicName, int particao, RastreadorEvent evento)
+        {
             try
             {
-                var config = new ProducerConfig
-                {
-                    BootstrapServers = bootstrapServers
-                };
+                var topicPart = new TopicPartition(topicName, new Partition(particao));
+                var result = await producer.ProduceAsync(
+                    topicPart,
+                    new() { Value = JsonSerializer.Serialize(evento) });
 
-                using (var producer = new ProducerBuilder<Null, string>(config).Build())
-                {
-                    for (int i = 2; i < args.Length; i++)
-                    {
-                        var result = await producer.ProduceAsync(
-                            nomeTopic,
-                            new () { Value = args[i] });
-
-                        logger.Information(
-                            $"Mensagem: {args[i]} | " +
-                            $"Status: { result.Status.ToString()}");
-                    }
-                }
-
-                logger.Information("Concluído o envio de mensagens");
             }
             catch (Exception ex)
             {
-                logger.Error($"Exceção: {ex.GetType().FullName} | " +
+                Console.WriteLine($"Exceção: {ex.GetType().FullName} | " +
                              $"Mensagem: {ex.Message}");
+            }
+        }
+
+        static async Task CreateTopic(string bootstrapServers, string topicName, int numPartitions)
+        {
+            using (var adminClient = new AdminClientBuilder(new AdminClientConfig { BootstrapServers = bootstrapServers }).Build())
+            {
+                try
+                {
+                    await adminClient.CreateTopicsAsync(new TopicSpecification[] {
+                    new TopicSpecification { Name = topicName, ReplicationFactor = 1, NumPartitions = numPartitions} });
+                }
+                catch (CreateTopicsException e)
+                {
+                    Console.WriteLine($"An error occured creating topic {e.Results[0].Topic}: {e.Results[0].Error.Reason}");
+                }
             }
         }
     }
---
+++ EnvioKafka/RastreadorEvent.cs
@@ -0,0 +1,18 @@
+﻿namespace EnvioKafka
+{
+    public class RastreadorEvent
+    {
+        public long Rastreador { get; set; }     
+        public int Contador { get; set; }
+
+        public RastreadorEvent(long rastreador)
+        {
+            Rastreador = rastreador;
+        }
+
+        public void Incrementa()
+        {
+            Contador++;
+        }
+    }
+}
